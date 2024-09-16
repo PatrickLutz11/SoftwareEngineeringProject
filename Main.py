5ba85410210744858edfc144ee52110300f6a722
PatrickLutz11 <163037264+PatrickLutz11@users.noreply.github.com>
2024-09-16 15:43:27 +0200

    Integrate Camera

--- Main.py
+++ Main.py
@@ -2,3 +2,21 @@
 ##Group B
 ##
 ##
+
+class Camera:
+
+    def __init__(self) -> None:
+        pass
+        
+        
+    def open(self, port:str) -> bool:
+        """
+        """
+
+    def close(self) -> None:
+        pass
+
+
+if __name__ == "__main__" :
+    camera = Camera()
+    camera.open(port="COM1")
