48658e012f722a089a770b1cf7a361782533d8ea
99Jannis <72573490+99Jannis@users.noreply.github.com>
2024-10-21 19:46:59 +0200

    update data selector and camera handling - data selector now works with camera handling - camera handling was further improved - removed deprecated files (file_handling.py and webcam.py)

--- src/data_selector.py
+++ src/data_selector.py
@@ -1,32 +1,100 @@
 """
-@data:      data_selector.py
-@author:    Jannis Mathiuet
-@versions:  ver 0.0.0 - 30.09.2024 - Jannis Mathiuet
-
-@desc: 
-    programm to select which data channel should be used.
-    If there are any questions or problems, please contact me.
+programm to select data channel and get data stream of selected channel.
 """
-import file_handling
-import webcam
+import handling_cameras
+import handling_paths_files
+import cv2
 
-class DataSelector:
-    def __init__(self)->int:
-        return 0
+class DataStream:
+    _camera_keywords = ["c", "camera", "cam"]
+    _folder_keywords = ["f", "folder"]
     
-    def select_channel(self, channel:str='f')->int:
+    def __init__(self)->None:
+        self.current_image = None
+        self.selected_channel = 'c'
+        
+        self.cam_op = handling_cameras.CameraOperator()
+        #self.fol_op = handling_paths_files.FileHandling()
+        
+        
+    
+    
+    def select_channel(self, channel:str='f')->bool:
         """selects and sets the data channel for the input.
 
         Args:
             channel (str): The channels where to take an image from
+                ["c", "camera", "cam"]: to select camera as data channel
                 ["f", "folder"]:        to select folder as data channel
-                ["w", "webcam", "cam"]: to select camera as data channel
+                
         Returns:
-            int: 0 if successful, failed otherwise.
+            bool: True, if successful, False otherwise.
         """
-        if channel in ["f", "folder"]:
-            print("folder input selected")
-        
-        if channel in ["w", "webcam", "cam"]:
+        if channel in self._camera_keywords:
             print("camera input selected")
-        return 0
+            self.channel_selected = 'c'
+            return True
+        
+        if channel in self._folder_keywords:
+            print("folder input selected")
+            self.channel_selected = 'f'
+            return True
+        
+        return False
+    
+    
+    def open_data_stream(self)->bool:
+        if self.channel_selected in self._camera_keywords:
+            self.cam_op.select_camera_device()
+            self.cam_op.open_camera_stream()
+            return True
+        
+        if self.channel_selected in self._folder_keywords:
+            # TODO implement folder
+            print("Folder tbd implemented")
+            return True
+        return False
+    
+    def close_data_stream(self)->bool:
+        if self.channel_selected in self._camera_keywords:
+            self.cam_op.close_camera_stream()
+            return True
+        
+        if self.channel_selected in self._folder_keywords:
+            # TODO implement folder
+            print("Folder tbd implemented")
+            return True
+        return False
+        
+    def get_current_image(self)->cv2.typing.MatLike:
+        if not self._update_current_image():
+            print("ERROR: Cannot update iamge!")
+            return None
+        return self.current_image
+    
+    
+    def _update_current_image(self)->bool:
+        if self.channel_selected in self._folder_keywords:
+            # TODO implement folder
+            print("Folder tbd implemented")
+            return True
+            
+        if self.channel_selected in self._camera_keywords:
+            self.current_image = self.cam_op.get_image_camera()
+            return True
+        return False
+
+def main():
+    ds = DataStream()
+    ds.select_channel('c')
+    is_opened = ds.open_data_stream()
+    while(is_opened):
+        img = ds.get_current_image()
+        cv2.imwrite("test.png", img)
+        cv2.imshow("q: end stream", img)
+        if cv2.waitKey(1) == ord('q'):
+            break
+    ds.close_data_stream()
+    
+if __name__ == "__main__":
+    main()
--- src/handling_cameras.py
+++ src/handling_cameras.py
@@ -1,31 +1,138 @@
 import numpy as np
 import cv2
+from typing import List, Tuple
+
+class CameraSearch:
+    def list_camera_devices(self)->List:
+        # src : https://stackoverflow.com/questions/57577445/list-available-cameras-opencv-python
+        is_working = True
+        device_port = 0
+        working_ports = []
+        available_ports = []
+        
+        while is_working:
+            #TODO catch error, so it's not put on console
+            camera = cv2.VideoCapture(device_port)
+            if not camera.isOpened():
+                is_working = False
+                print("Port %s is not working." %device_port)
+                camera.release()
+                
+            else:
+                is_reading, img = camera.read()
+                width_camera = camera.get(3)
+                height_camera = camera.get(4)
+                
+                if is_reading:
+                    print("Port %s is working and reads images (%s x %s)" %(device_port,height_camera,width_camera))
+                    working_ports.append(device_port)
+                else:
+                    print("Port %s for camera ( %s x %s) is present but does not reads." %(device_port,height_camera,width_camera))
+                    available_ports.append(device_port)
+            device_port +=1
+        return working_ports
+
+
 
 class CameraOperator:
-    def __init__(self):
-        self.camera = CameraDevice()
-        print(self.camera)
-        self._capture = cv2.VideoCapture
+    def __init__(self, _camera_device_port:int=0)->None:
+        self.camera_device_port = 0
+        if _camera_device_port >= 0:
+            self.camera_device_port = _camera_device_port
+        
+        self._capture = cv2.VideoCapture(self.camera_device_port)
+        self._stream_opened = False
+        self._camera_available = True
     
     
-    def set_camera_device(self, _camera_device):
-        self.camera = _camera_device
+    def select_camera_device(self)->bool:
+        msg_not_found_port = f"Port could not be found. Port %s remains." %(self.camera_device_port)
+        working_device_ports = CameraSearch().list_camera_devices()
+        if 0 not in working_device_ports:
+            self._camera_available = False
+            return False
+        
+        print("\r\nEnter one of the following numbers to select camera port:", 
+              working_device_ports)
+        try: 
+            user_number_input = int(input())
+        except:
+            print("ERROR: No valid number!", msg_not_found_port)
+            return False
+            
+        if user_number_input in working_device_ports:
+            print(f"Port {user_number_input} is selected")
+            self.camera_device_port = user_number_input
+            return True
+        else: 
+            print(msg_not_found_port)
+        return False
+
     
-    
-    def open_camera_stream(self) -> int:
+    def open_camera_stream(self) -> bool:
         temp_cap = self._capture
-        self._capture = cv2.VideoCapture(self.camera.device_id)
+        self._capture = cv2.VideoCapture(self.camera_device_port)
         
         if not self._capture.isOpened():
             print("ERROR: Cannot open camera")
             self._capture = temp_cap
             exit()
-            return -1
+            return False
+        self._stream_opened = True
+        return True
+    
+    
+    def close_camera_stream(self)->None:
+        self._capture.release()
         
-        return 0
+    
+    def get_image_camera(self)->cv2.typing.MatLike:
+        if not self._stream_opened:
+            self.open_camera_stream()
+        
+        retrived, frame = self._capture.read()
+        if not retrived:
+            print("ERROR: Can't receive frame (stream end?). Exiting ...")
+            self._stream_opened = False
+            return None
+        
+        return frame
+        
+
+    
+
+
+
+        
+
+    
+
+def main():
+    cam_op = CameraOperator()
+    #cam_op.select_camera_device()
+    cam_op.open_camera_stream()
+    images = []
+    for i in range(0,10):
+        image = cam_op.get_image_camera()
+        images.append(image)
+        cv2.imshow(f"{i}) q:close windows", image)
+        cv2.waitKey(100)
+    if cv2.waitKey(0) == ord('q'):
+        cam_op.close_camera_stream
+    print(images)
     
     
-    def run_camera_stream(self) -> None:
+    
+if __name__ == "__main__":
+    main()
+    
+    
+    
+    
+    
+    
+    """
+        def run_camera_stream(self) -> None:
         frame_delay_millis = int(1000/self.camera.frames_per_second)
         self.open_camera_stream()
         
@@ -41,11 +148,11 @@ class CameraOperator:
                 break
         self._capture.release()
         cv2.destroyAllWindows()
+    """
     
-
-
-class CameraDevice:
-    def __init__(self, _device_id:int=0, _fps:float=1000):
+    """
+    class CameraDevice:
+    def __init__(self, _device_id:int=0, _fps:float=1000)->None:
         self.device_id = _device_id
         self.frames_per_second = _fps
         
@@ -79,13 +186,4 @@ class CameraDevice:
         
     def __str__(self):
         return f'Camera(ID:{self.device_id}, {self.frames_per_second}fps)'
-        
-
-    
-
-def main():
-    cam_op = CameraOperator()
-    cam_op.run_camera_stream()
-    
-if __name__ == "__main__":
-    main()
+    """
