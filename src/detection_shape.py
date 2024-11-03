f602e54ba9b4917af742d7ff9d9f5334e9c31115
99Jannis <72573490+99Jannis@users.noreply.github.com>
2024-11-03 11:10:19 +0100

    update detection shape - filter now excluded from detection an in seperate class - center point is seperate function

--- src/detection_shape.py
+++ src/detection_shape.py
@@ -29,41 +29,14 @@ class Detection:
         thresholded = cv2.adaptiveThreshold(blurred, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
         
         contours, _ = cv2.findContours(thresholded, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        found_shapes = sorted(contours, key=cv2.contourArea, reverse=True) # excluding background
+        found_shapes = sorted(contours, key=cv2.contourArea, reverse=True)[1:] # excluding background
         
-        filtered_found_shapes = []
-        center_points = [(0,0)]
-        for shape in found_shapes:
-            area = cv2.contourArea(shape)
-            if miniumum_area_for_shape > area:
-                break
-            
-            center_point_new = Detection.get_shape_center(shape)
-            
-            center_exists:bool = False
-            for center in center_points:
-                x_delta = abs(center[0]-center_point_new[0])
-                y_delta = abs(center[1]-center_point_new[1])
-                if x_delta <= 2 or y_delta <= 2:
-                    center_exists = True
-            if center_exists == True:
-                continue
-            
-            center_points.append(center_point_new)
-            filtered_found_shapes.append(shape)
-        """
-         print(center_point_new, center_points)
-                print(len(filtered_found_shapes))
-                print(f"\r\n")
-        """
-                #continue
-            #print(center_point_new)
-            #center_points.append(center_point_new)
-            #filtered_found_shapes.append(shape)
+        filtered_shapes = FilterShapes.minimum_shape_size(found_shapes, 
+                                                          miniumum_area_for_shape)
+        filtered_shapes = FilterShapes.minimum_center_distance(filtered_shapes,
+                                                               miniumum_distance=2)
         
-        
-        # Debugging 
-        if False: 
+        if False: # Debugging 
             cv2.imshow("gray", gray_img)
             cv2.imshow("blurred", blurred)
             cv2.imshow("thresholded", thresholded)
@@ -73,7 +46,7 @@ class Detection:
             print(len(found_shapes))
             print(len(filtered_found_shapes))
         
-        return filtered_found_shapes 
+        return filtered_shapes 
 
             
     def shape_recognition(found_shapes:List, img:cv2.typing.MatLike) -> List[Dict[str, str]]:
@@ -87,24 +60,10 @@ class Detection:
             List[Dict[str, str]]: List of recognized shapes with pattern and color
         """
         recognized_shapes = []  # List to store recognized shapes
-        print("Shape detection:", len(found_shapes))
-        i = 0
-        for shape in found_shapes:
-            if i == 0:
-                i = 1
-                continue
-            
+        for shape in found_shapes:            
             define_shape = cv2.approxPolyDP(shape, 0.01 * cv2.arcLength(shape, True), True)
-            print(len(define_shape))
-            i += 1
             shape_color = ColorDetector().get_color(img, shape)
             shape_name = "Circle"
-            """
-            shape_points = cv2.moments(shape) 
-            if shape_points['m00'] != 0.0: 
-                x = int(shape_points['m10']/shape_points['m00']) - 100
-                y = int(shape_points['m01']/shape_points['m00']) 
-            """
             
             if len(define_shape) == 3:
                 shape_name = "Triangle"
@@ -149,7 +108,7 @@ class Detection:
                 cv2.drawContours(img, [shape], 0, BGR_COLORS["BLACK"], 5)
                 cv2.putText(img, f'Circle, {shape_color}', (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 255), 2)
             """
-            x_coord_text, y_coord_text = Detection.get_shape_center(shape)
+            x_coord_text, y_coord_text = OperationShapes.get_shape_center(shape)
             cv2.drawContours(img, [shape], 0, BGR_COLORS["CYAN"], 5)
             print(shape_name, shape_color, (x_coord_text, y_coord_text))
             text = f'{shape_name}, {shape_color}'
@@ -167,6 +126,45 @@ class Detection:
         print(f"\r\n\n")
         return recognized_shapes
     
+
+class FilterShapes:
+    @staticmethod
+    def minimum_shape_size(found_shapes:List[cv2.typing.MatLike], 
+                                  miniumum_area_for_shape:int
+                                  )->List[cv2.typing.MatLike]:
+        filtered_found_shapes = []
+        for shape in found_shapes:
+            area = cv2.contourArea(shape)
+            if miniumum_area_for_shape > area:
+                break
+            filtered_found_shapes.append(shape)
+        return filtered_found_shapes
+    
+    @staticmethod
+    def minimum_center_distance(found_shapes:List[cv2.typing.MatLike], 
+                                      miniumum_distance:int=2
+                                      )->List[cv2.typing.MatLike]:
+        filtered_found_shapes = []
+        center_points = [(0,0)]
+        
+        for shape in found_shapes:
+            center_point_new = OperationShapes.get_shape_center(shape)
+            
+            center_exists:bool = False
+            for center in center_points:
+                x_delta = abs(center[0]-center_point_new[0])
+                y_delta = abs(center[1]-center_point_new[1])
+                if (x_delta <= miniumum_distance) or (y_delta <= miniumum_distance):
+                    center_exists = True
+            
+            if center_exists == True:
+                continue
+            center_points.append(center_point_new)
+            filtered_found_shapes.append(shape)
+        return filtered_found_shapes
+
+class OperationShapes:
+    @staticmethod
     def get_shape_center(mask_shape:cv2.typing.MatLike)->Tuple[int, int]:
         shape_points = cv2.moments(mask_shape) 
         x_coord = 0
@@ -175,9 +173,7 @@ class Detection:
             x_coord = int(shape_points['m10']/shape_points['m00'])
             y_coord = int(shape_points['m01']/shape_points['m00']) 
         return x_coord, y_coord
-
-
-
+    
 if __name__ == "__main__": 
     """Debugging of functions
     """
